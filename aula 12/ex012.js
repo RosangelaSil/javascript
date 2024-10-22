93d6d2f1360cc72bfbd9a35a894f98a09dd58679
RosangelaSil <rosangela3000@gmail.com>
2024-10-22 21:38:28 +0100

    Exercicios

---
+++ aula 12/ex012.js
@@ -0,0 +1,10 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+    if (hora <12) {
+    console.log(`Bom dia flor do dia!`)
+} else if (hora <= 18) {
+    console.log(`Boa tarde princesa!`)
+}else {
+    console.log(`Good night babe`)
+}
