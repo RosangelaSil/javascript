3990c2f47f41f25eae76cb4d901063644dc05517
RosangelaSil <rosangela3000@gmail.com>
2024-10-28 17:30:00 +0000

    atualização

---
+++ aula 15/ambriente.js
@@ -0,0 +1,9 @@
+let num = [ 2,8,7,3,9]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`) 
+let pos = num.indexOf(4)
+console.log(`O valor 8 está na posição ${pos}`)
+
